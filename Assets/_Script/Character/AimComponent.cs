2804971a95a3a2dc4059dea259a4a6a8e3c1281f
Stephen Ho <stephen.ho.232@gmail.com>
2023-10-10 12:40:58 +0700

    Add atk vfx + atk damage area

--- Assets/_Script/Character/AimComponent.cs
+++ Assets/_Script/Character/AimComponent.cs
@@ -11,8 +11,44 @@ namespace JustGame.Script.Common
         [SerializeField] private Vector2 m_aimDirection;
         [SerializeField] private float m_angle;
         [SerializeField] private float m_offsetAngle;
-        
+        private Vector2 m_clampAimDirection;
         public Vector2 AimDirection => m_aimDirection;
+        public Vector2 ClampAimDirection  
+        {
+            get
+            {
+                float x = 0;
+                float y = 0;
+                if (m_aimDirection.x < -0.5f)
+                {
+                    x = -0.5f;
+                }
+                else if (m_aimDirection.x > 0.5f)
+                {
+                    x = 0.5f;
+                }
+                else
+                {
+                    x = 0;
+                }
+                
+                if (m_aimDirection.y < -0.5f)
+                {
+                    y = -0.5f;
+                }
+                else if (m_aimDirection.y > 0.5f)
+                {
+                    y = 0.5f;
+                }
+                else
+                {
+                    y = 0;
+                }
+
+                return new Vector2(x, y);
+            }
+        }
+
         public float Angle => m_angle;
         
         private InputManager m_inputManager;
--- Assets/_Script/Character/Player/PlayerWeaponHandler.cs
+++ Assets/_Script/Character/Player/PlayerWeaponHandler.cs
@@ -1,4 +1,5 @@
-using System;
+using System.Collections;
+using JustGame.Script.Common;
 using JustGame.Scripts.Managers;
 using UnityEngine;
 
@@ -6,8 +7,12 @@ namespace JustGame.Script.Character
 {
     public class PlayerWeaponHandler : MonoBehaviour
     {
+        [SerializeField] private AimComponent m_aimComponent;
+        [SerializeField] private float m_weaponRange;
+        [SerializeField] private BoxCollider2D m_weaponDamageArea;
+        [SerializeField] private AnimationParameter m_atkAnim;
         private InputManager m_inputManager;
-
+        private bool m_atkInProgress;
         private void Start()
         {
             m_inputManager = InputManager.Instance;
@@ -22,15 +27,33 @@ namespace JustGame.Script.Character
                 WeaponStart();
             }
         }
-
+        
         public void WeaponStart()
         {
-            
+            StartCoroutine(AttackRoutine());
         }
 
+        private IEnumerator AttackRoutine()
+        {
+            if (m_atkInProgress)
+            {
+                yield break;
+            }
+            m_atkInProgress = true;
+            var atkPos = (Vector2)transform.position + m_aimComponent.ClampAimDirection * m_weaponRange;
+            m_weaponDamageArea.enabled = true;
+            m_weaponDamageArea.transform.position = atkPos;
+            m_atkAnim.SetTrigger();
+            yield return new WaitForSeconds(m_atkAnim.Duration);
+            m_weaponDamageArea.enabled = false;
+            m_weaponDamageArea.transform.localPosition = Vector2.zero;
+            m_atkInProgress = false;
+        }
+        
         public void WeaponStop()
         {
-            
+            m_atkInProgress = false;
+            StopAllCoroutines();
         }
     } 
 }
